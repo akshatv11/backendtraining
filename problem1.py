e2e93659008e304d6d42730a60a67e0ca37805e9
akshatv11 <108255454+akshatv11@users.noreply.github.com>
2022-07-19 13:34:12 +0530

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,72 @@
+# n = input('number of test cases:')
+# n = int(n)
+
+# for i in range(n):
+#     cal = input('enter the operation')
+#     cal_list = cal.split()
+#     k = len(cal_list)
+#     print(cal_list)
+#     if cal_list[0] =='add':
+#         sol = 0
+#         for j in cal_list[1: ], n:
+#             sol = sol
+
+            
+
+            
+    # elif cal_list[0] == 'sub':
+    #     num1 = int(cal_list[1])
+    #     num2 = int(cal_list[2])
+    #     print(num1 - num2)
+    # elif cal_list[0] == 'divide':
+    #     num1 = int(cal_list[1])
+    #     num2 = int(cal_list[2])
+    #     print(num1 / num2)
+    # elif cal_list[0] == 'mul':
+    #     num1 = int(cal_list[1])
+    #     num2 = int(cal_list[2])
+    #     print(num1 * num2)
+    
+
+
+
+
+
+# factorial
+# a = int(input('enter a number'))
+
+# f = 1
+
+# while (a > 0):
+#     f = f * a
+#     a = a - 1
+
+# print(f)
+
+
+
+
+# palindrome
+
+# a = input('enter a string')
+# b = a[::-1]
+# print(b)
+
+
+# if (a == b):
+#     print('palindrome')
+# else:
+#     print('not a palindrome')
+
+
+
+
+
+
+
+
+    
+
+
+
+    
